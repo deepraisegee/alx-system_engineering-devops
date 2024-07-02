caec48d725488dbaf1e31c7d18f12bdbd851152a
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-07-02 16:13:45 +0000

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# install flask from pip3.
+
+$deps = ['python3', 'python3-pip']
+
+package { $deps:
+    ensure  => installed,
+}
+
+exec { 'install flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+}
