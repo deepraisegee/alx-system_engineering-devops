e3c9f6b7434999a28e591732c3814f9d1be434ce
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-08-26 01:13:56 +0100

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Script to make an API call to Reddit API"""
-
-import requests as req
+"""
+Queries the Reddit API and returns
+the number of subscribers
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number"""
-    api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "praisegee"}
-
-    res = req.get(api_url, headers=headers, allow_redirects=False)
-
-    if res.status_code != 200:
+    headers = {'User-agent': 'praisegee'}
+    URL = 'https://www.reddit.com/r/{}/about.json'
+    get_sudreddit = requests.get(URL.format(subreddit),
+                                 headers=headers, allow_redirects=False)
+    if get_sudreddit.status_code != 200:
         return 0
-    return res.json().get("data", {}).get("subscribers")
+    return get_sudreddit.json().get('data', {}).get('subscribers')
